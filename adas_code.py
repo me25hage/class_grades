458ed2c95a9d499a049c99aaddfe8f6b014b8508
me25hage <me25hage@siena.edu>
2015-12-08 11:07:51 -0500

    Another small edit

--- adas_code.py
+++ adas_code.py
@@ -1,6 +1,6 @@
 def read_data(x):
     import numpy as np
-    columns = np.loadtxt(x,delimiter=',',skiprows=1,unpack=True,dtype='int')
+    columns = np.loadtxt(x,delimiter=',',unpack=True,dtype='int')
     grades = columns[1]
     return grades
 
